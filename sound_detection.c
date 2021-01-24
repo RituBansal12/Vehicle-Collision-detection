51339cbf383546d14c7a6088cd10819a5ad77d90
RituBansal12 <75537428+RituBansal12@users.noreply.github.com>
2021-01-24 22:58:05 +0530

    Create sound_detection.c

---
+++ sound_detection.c
@@ -0,0 +1,39 @@
+//the microphone amplifier output is connected to pin A0
+const int MIC = 0;
+
+//the variable that will hold the value read from the microphone each time
+int adc;
+int dB, PdB; 
+
+void setup() {
+//sets the baud rate at 9600 so we can check the values the microphone is obtaining on the Serial Monitor
+Serial.begin(9600); 
+pinMode(3, OUTPUT);
+}
+
+void loop(){
+
+//store previous dB
+PdB = dB;
+
+//Read the ADC value from amplifer  
+adc= analogRead(MIC);
+
+//Convert ADC value to dB using Regression values
+dB = (adc+83.2073) / 11.003; 
+
+if (PdB!=dB)
+Serial.println (dB);
+
+if (dB>60)
+{
+// turn the LED on (HIGH is the voltage level)
+  digitalWrite(3, HIGH);   
+// wait for a second
+  delay(2000);                       
+  digitalWrite(3, LOW); 
+}
+
+//delay(100);
+
+}
